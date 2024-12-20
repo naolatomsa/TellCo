b35aa0d2f947700d746589327f979e1f21064174
naolatomsa <naolatomsa4@gmail.com>
2024-12-20 16:51:24 +0300

    treat outliers, variable transformation, analyse basic matrics, non graphical univariate analysis

---
+++ scripts/variableTransformation.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+def variable_transformation(user_aggregated):
+    # Create decile segments based on total session duration
+    user_aggregated['Decile'] = pd.qcut(user_aggregated['Dur. (ms)'], 5, labels=False)
+
+    # Compute total data volume (DL + UL) per decile
+    decile_data = user_aggregated.groupby('Decile').agg({
+        'Total Data Volume': 'sum',
+        'Dur. (ms)': 'mean'
+    }).reset_index()
+    return decile_data;
+
+def plot_variable_transformation(decile_data):
+    sns.barplot(data=decile_data, x='Decile', y='Total Data Volume')
+    plt.title('Total Data Volume Across Deciles')
+    plt.xlabel('Decile (Based on Session Duration)')
+    plt.ylabel('Total Data Volume (Bytes)')
+    plt.show()
